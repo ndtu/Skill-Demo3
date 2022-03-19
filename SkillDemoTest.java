32a0349558e2a0a74033a0f16cdf754c5631a781
ndtu <77379218+ndtu@users.noreply.github.com>
2022-03-18 17:15:53 -0700

    test

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    public void subtraction1() {
+        @Test
+        assertEquals(3, SkillDemo.subtract(6,2));
+    }
+}
